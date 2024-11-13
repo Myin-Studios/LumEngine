93ee0772e6aff7fa9a35f7aba2bd15f193e611f1
Dnurr <ste.russo035@gmail.com>
2024-11-13 15:23:48 +0100

    Working on third phase of interop between C++/C#

--- LumEngine/Engine/GUI/Rendering/Shaders/Shaders.h
+++ LumEngine/Engine/GUI/Rendering/Shaders/Shaders.h
@@ -8,13 +8,13 @@ public:
 	ShaderCore(const char* vertexPath, const char* fragmentPath);
 	~ShaderCore();
 
-    void use() const;
-    // utility uniform functions
-    void setBool(const std::string& name, bool value) const;
-    void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, float value) const;
-    void setFloatArray(const std::string& name, GLsizei count, const float* value) const;
-    void setVec3(const std::string& name, glm::vec3 v3) const;
-    void setVec3Array(const std::string& name, GLsizei count, const float* value) const;
-    void setMat4x4(const std::string& name, const float* value) const;
+    using Shader::use;
+
+    using Shader::setBool;
+    using Shader::setInt;
+    using Shader::setFloat;
+    using Shader::setFloatArray;
+    using Shader::setVec3;
+    using Shader::setVec3Array;
+    using Shader::setMat4x4;
 };
